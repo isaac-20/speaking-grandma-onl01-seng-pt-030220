0ef452ddff83867798ba2c48d72a026592e8edf1
Isaac Smith <isaacsmith215@aol.com>
2020-03-07 21:44:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase="Hey Grandma")
+def speak_to_grandma(phrase="")
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "I LOVE YOU"
+  elsif phrase == "I LOVE YOU"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else phrase = phrase.upcase
+  else phrase == phrase.upcase
     puts "NO, NOT SINCE 1938"
   end
 end
